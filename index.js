174cec662d3d6e333b235d2c249205fe7ea6198d
LawCR <alvacalderon99@gmail.com>
2021-10-17 14:53:29 -0500

    added settings

---
+++ index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const app = require("./app")
+const PORT_SERVER = process.env.PORT || 8081
+const { API_VERSION, IP_SERVER, PORT_DB, DB} = require("./config")
+
+const DB_URL = `mongodb://${IP_SERVER}:${PORT_DB}/${DB}`
+const SERVER_URL = `http://${IP_SERVER}:${PORT_SERVER}/api/${API_VERSION}/`
+
+mongoose.connect(DB_URL,
+    {useNewUrlParser:true},
+    (err,res)=>{
+        if (err) {
+            throw err;
+        } else {
+            console.log("La conexion a la base de datos es correcta.");
+            app.listen(PORT_SERVER, () => {
+                console.log("#####################");
+                console.log("###### API REST #####");
+                console.log("#####################");
+                console.log(SERVER_URL)
+            })
+        }
+    }
+    )
